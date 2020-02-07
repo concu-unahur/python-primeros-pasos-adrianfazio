cedc5fb4655c5465bd5a5274da6c773272c49477
Adrian Fazio <adrian.fazio@outlook.com>
2020-02-06 21:33:45 -0300

    Ejercicio1-parte2

--- operaNoConm.py
+++ operaNoConm.py
@@ -4,5 +4,39 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+num = 1
+lock = threading.Lock()
+
+def sumarUno():
+    global num
+    global lock
+    try:
+        lock.acquire()
+        num = num + 1
+    finally:
+        lock.release()
+    
+
+def multiplicarPorDos():
+    global num
+    global lock
+    try:
+        lock.acquire()
+        num = num * 2
+    finally:
+        lock.release()
+    
+    
+
+t1 = threading.Thread(target=sumarUno, name='Sumar 1')
+t2 = threading.Thread(target=multiplicarPorDos, name='MultiplicarPorDos')
+
+t2.start()
+t1.start()
+
+print(f'Cantidad vale {num}')
+
+
+
 
 
