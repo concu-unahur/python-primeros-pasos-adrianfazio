d82b33462555595ee5bb9701a618eb0d8cc9e8a1
Adrian Fazio <adrian.fazio@outlook.com>
2020-02-09 13:49:21 -0300

    Parte 2 /1

--- definiciones.py
+++ definiciones.py
@@ -1,7 +1,20 @@
-import clasesYfunciones
 import time
 import threading
 import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+def dormir():
+    logging.info('Empenzado')
+    time.sleep(1)
+    logging.info('Finalizado')
+
+class UnThread(threading.Thread):
+    def __init__(self):
+        super().__init__() 
+        self.name = 'threadClase'
+
+    def run(self):
+        logging.info('Empezando')
+        time.sleep(1)
+        logging.info('Finalizado')
--- operaNoConm.py
+++ operaNoConm.py
@@ -25,16 +25,41 @@ def multiplicarPorDos():
         num = num * 2
     finally:
         lock.release()
-    
-    
+
+def unoDivididoDos():
+    global num
+    num = 1/2
+
+def tresDivididoDos():
+    global num
+    num = 3/2
+
+def primeraCuenta():
+    global num
+    tprimero = threading.Thread(target=sumarUno)
+    tprimero.start()
+    tprimero.join()
+    tprimero.start()
+    tprimero.join()
+    tprimero.start()
+    tprimero.join()
+    print(f'Cantidad vale {num}')
+
 
 t1 = threading.Thread(target=sumarUno, name='Sumar 1')
 t2 = threading.Thread(target=multiplicarPorDos, name='MultiplicarPorDos')
+t3 = threading.Thread(target=unoDivididoDos, name='unoDivididoDos')
+t4 = threading.Thread(target=tresDivididoDos, name='tresDivididoDos')
+t5 = threading.Thread(target=primeraCuenta)
 
-t2.start()
-t1.start()
+#t2.start()
+#t1.start()
+#t3.start()
+#t4.start()
+t5.start()
 
 print(f'Cantidad vale {num}')
+
 
 
 
